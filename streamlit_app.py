ef9e1c4229df4a46bdaf49f12c0c5b97e71727e8
offx-krisjay <161944058+offx-krisjay@users.noreply.github.com>
2025-08-18 10:36:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,11 @@ if up_file is not None:
         filt_val=st.sidebar.text_input("enter filter value")
         if filt_val:
             df=df[df[filt_col].astype(str).str.contains(filt_val)]
+            clean_df=us_df
+            st.dataframe(clean_df)
+                st.download_button("Cleaned Data",clean_df.to_csv(Index=False),"cleaned_data.csv")
+
+    
         
     st.subheader("Cleaned Data Preview")
     if df.empty:
